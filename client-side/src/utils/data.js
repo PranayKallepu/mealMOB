ec744621dc3c73cd6de336d99b64564031f19f77
Pranay Kallepu <pranaykallepu05@gmail.com>
2025-02-20 13:47:18 +0530

    uncomment localhost

--- client-side/src/utils/data.js
+++ client-side/src/utils/data.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://localhost:4000";
-// export const API_URL = "https://mealmob-server.onrender.com";
+// export const API_URL = "http://localhost:4000";
+export const API_URL = "https://mealmob-server.onrender.com";
 
 export const menuList = [
   {
@@ -53,67 +53,60 @@ export const menuList = [
   },
   {
     item_id: 8,
-    cuisine: "Chinese",
-    item_img: "/assets/menuItems/Shawarma.avif",
-    description:
-      "Try our juicy and flavorful shawarmas, packed with exotic spices.",
-  },
-  {
-    item_id: 9,
     cuisine: "Pizza",
     item_img: "/assets/menuItems/pizza.avif",
     description:
       "Delight in cheesy, oven-fresh pizzas topped with your favorite ingredients.",
   },
   {
-    item_id: 10,
+    item_id: 9,
     cuisine: "North-Indian",
     item_img: "/assets/menuItems/North_Indian_4.avif",
     description:
       "Taste the authentic flavors of North India, rich in spices and aroma.",
   },
   {
-    item_id: 11,
+    item_id: 10,
     cuisine: "Tiffins",
     item_img: "/assets/menuItems/Vada.avif",
     description:
       "Enjoy crispy, golden vadas served with hot and flavorful sambar.",
   },
   {
-    item_id: 12,
+    item_id: 11,
     cuisine: "Tiffins",
     item_img: "/assets/menuItems/Paratha.avif",
     description:
       "Flaky, buttery parathas paired perfectly with delicious curries.",
   },
   {
-    item_id: 13,
+    item_id: 12,
     cuisine: "Tiffins",
     item_img: "/assets/menuItems/Poori.avif",
     description: "Soft, puffed pooris served with tasty side dishes.",
   },
   {
-    item_id: 14,
+    item_id: 13,
     cuisine: "Shakes",
     item_img: "/assets/menuItems/Shake.avif",
     description:
       "Refresh yourself with thick, creamy Shakes in various flavors.",
   },
   {
-    item_id: 15,
+    item_id: 14,
     cuisine: "Tiffins",
     item_img: "/assets/menuItems/Bonda.avif",
     description: "Crispy and delicious Bonda, a perfect tea-time snack.",
   },
   {
-    item_id: 16,
+    item_id: 15,
     cuisine: "South-Indian",
     item_img: "/assets/menuItems/South_Indian.avif",
     description:
       "Taste these delectable classics, delectable South Indian Specials to make your day.",
   },
   {
-    item_id: 17,
+    item_id: 16,
     cuisine: "North-Indian",
     item_img: "/assets/menuItems/North_Indian_4.avif",
     description:
